157285d4ca4e61154c5b29244a155054ed8bbfb0
MathiusMendoza <mendozamathius9@gmial.com>
2023-12-13 16:06:47 -0500

    ok

--- Ejemplos/src/paquete1/Ejemplo03.java
+++ Ejemplos/src/paquete1/Ejemplo03.java
@@ -19,13 +19,20 @@ public class Ejemplo03 {
 
         // arreglo de tipo entero
         int[] arreglo1 = new int[5];
+        // La diferencia del arrgeglo uno esque que esta es una manera formal de 
+        // incicar o declarar un arreglo.
         
         // arreglo de tipo entero
         int[] arreglo2;
         arreglo2 = new int[5];
+        // La diferencia del arreglo dos es que esta en la primera linea declarando 
+        // la varibale del arreglo y en la segunda linea le esta dando los elemenos
+        // a guardar en el mismo.
         
         // arreglo de tipo entero
         int[] arreglo3 = {5, 10, 15, 20, 25}; // sin new
+        // La diferencia del presente arreglo es que esta dando valores predeterminados 
+        // especificos mediante llaves.
         
         // arreglo de doubles - 3 elementos
         double[] arreglo4 = {5.1, 10.2, 15.2}; // sin new
